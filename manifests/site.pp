ebfaf937e332f95b8d0024cc21562426309d269f
Hardik <36600312+hardik20@users.noreply.github.com>
2018-02-25 20:04:30 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,7 @@
 node default {
   file {'/root/README":
-  ensure => file, 
-  content => 'This is  my puppet readme. ,
+  ensure   => file, 
+  content  => 'This is  my puppet readme. ,
+  owner    => 'root',
   }
  }
